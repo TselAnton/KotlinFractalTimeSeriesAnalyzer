1d5b077487d89c38498242ea6d8e9d912c569940
Anton_Tsel <anton_tsel@epam.com>
2021-01-28 20:04:31 +0400

    Change to logarithmic MNK

--- src/main/kotlin/math/MNKCalculator.kt
+++ src/main/kotlin/math/MNKCalculator.kt
@@ -1,23 +1,23 @@
 package math
 
-import kotlin.math.atan
+import kotlin.math.ln
 import kotlin.math.pow
-import kotlin.math.tan
 
 fun getMNKArguments(points: List<Pair<Double, Double>>) {
-    val sumX = points.map { pair -> pair.first }.sum()
-    val sumY = points.map { pair -> pair.second }.sum()
-    val sumXY = points.map { pair -> pair.first * pair.second }.sum()
-    val sumX2 = points.map { pair -> pair.first.pow(2) }.sum()
-    val n = points.size
+    val linerArgs = findLinerArguments(points)
+    println(linerArgs)
+}
+
+private fun findLinerArguments(points: List<Pair<Double, Double>>): Pair<Double, Double> {
+    val logPoints = points.map { pair -> Pair(ln(pair.first), ln(pair.second)) }
+    val sumX = logPoints.map { pair -> pair.first }.sum()
+    val sumY = logPoints.map { pair -> pair.second }.sum()
+    val sumXY = logPoints.map { pair -> pair.first * pair.second }.sum()
+    val sumX2 = logPoints.map { pair -> pair.first.pow(2) }.sum()
+    val n = logPoints.size
 
     val a = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX.pow(2))
     val b = (sumY - a * sumX) / n
 
-    //todo: доделать - посчитать H
-    println("a = $a, b = $b")
-    println(atan(a))
-    println(tan(a))
-    println(atan(b))
-    println(tan(b))
+    return Pair(a, b)
 }
